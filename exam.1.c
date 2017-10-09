0995c553ad49dfce1c11116aaa37736bd28075b4
7742 <32646821+7742@users.noreply.github.com>
2017-10-09 10:14:24 -0600

    Add files via upload

---
+++ exam.1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main() {
+    int i,x;
+x=0;
+  while(9>=x)
+  {
+  	 
+  
+  	 
+        printf("dame el primer numero para saber si es Par o Impar:\t");
+
+	scanf("%d",&i);
+
+    if (i%2==0)
+    {
+       printf("Es par.\n");
+    }
+    else
+    {
+       printf("Es impar.\n");
+    }  
+   x=x+1;
+    }
+
+return 0;
+}
+
